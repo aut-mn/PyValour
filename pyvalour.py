d9bfb46273bafe085988362d349e19fd83d9c473
Autumn <autumn@autmn.net>
2023-12-31 11:36:28 -0700

    Support for account creation, profile updating, non-tokenized requests

--- pyvalour.py
+++ pyvalour.py
@@ -1,13 +1,13 @@
+# This is the main library you import
 from AsyncLogger import AsyncLogCollector
 import valquest
 import asyncio
 
 # -- FRIENDS --
-# userfriends/add/{nameTag}  - Add friend
+# userfriends/add/{nameTag}  - Add/accept friend
 # userfriends/cancel/{nameTag}  - Cancel outgoing friend request
 # userfriends/decline/{nameTag}  - Decline friend request
 # userfriends/remove/{nameTag}  - Remove a friend
-
 async def add_friend(token:str, name:str, tag:str):
     status = await valquest.authenticated_request("POST", f"userfriends/add/{name}%23{tag}", token)
     if int(status[0]) == 201: return True
@@ -62,6 +62,11 @@ async def get_planet_members(token:str, planetId:int, pagination:int): # Returns
 
 # -- MEMBERS --
 # members/{userId}/roles  - Returns the Role ID for the specified user
+# userProfiles/{userId}  - Returns things like the headline, bio, etc.
+async def get_user_profile(token:str, userId:int):
+    status = await valquest.authenticated_request("GET", f"userProfiles/{userId}", token)
+    if int(status[0]) == 200: return status[1]
+    else: return False
 
 # -- SUBSCRIPTIONS --
 # subscriptions/{subscriptionType}/price  - Get the price of Valour subscription
@@ -86,7 +91,6 @@ async def get_unread_notifications(token:str): # Returns unread notifs JSON
 # channels/{channelId}/messages/{messageId}  - Edit a message
 # channels/{channelId}/messages/{messageId}  - Delete a message
 # users/self/statedata  - Retrieve channel states
-
 async def show_typing(token:str, channelId:int):
     status = await valquest.authenticated_request("POST", f"channels/{channelId}/typing", token)
     if int(status[0]) == 200: return True
@@ -102,15 +106,33 @@ async def get_direct_messages(token:str): # Returns DMs JSON
 
 # -- SELF --
 # users/self/compliance/{birthday}/{locality}  - Set the locality and birthday for the user
-# users/token  - Get authenticated Valour user token
+# users/token  - Create a new token from an email & password
 # users/self  - Get the authenticated user 
 # users/self/referrals  - Get referrals
+async def create_session(email:str, password:str): # Creates a session and returns a token and userId
+    status = await valquest.authenticated_request("POST", f"users/token", token=None, json={"email":email,"password":password})
+    if int(status[0]) == 200: return status[1]['id'], status[1]['userId']
+    else: return False
 
 async def get_authenticated_user(token:str): # Returns authenticated json
     status = await valquest.authenticated_request("GET", f"users/self", token)
     if int(status[0]) == 200: return status[1]
     else: return False
 
+async def update_profile(token:str, userId:int, animatedBorder:bool, backgroundImage:str, bio:str, borderColor:str, glowColor:str, headline:str, primaryColor:str, secondaryColor:str, tertiaryColor:str, textColor:str):
+    status = await valquest.authenticated_request("PUT", f"userProfiles/{userId}", token, json={"baseRoute":"api/userProfiles","headline":headline,"bio":bio,"borderColor":borderColor,"glowColor":glowColor,"primaryColor":primaryColor,"secondaryColor":secondaryColor,"tertiaryColor":tertiaryColor,"textColor":textColor,"animatedBorder":animatedBorder,"backgroundImage":backgroundImage,"id":userId})
+    if int(status[0]) == 200: return True
+    else: return False
+
+async def get_referrals(token:str):
+    status = await valquest.authenticated_request("GET", f"users/self/referrals", token)
+    if int(status[0]) == 200: return status[1]
+    else: return False
+
+async def create_account(username:str, email:str, password:str, DoB:str, referrer): # Creates an account. Yes. DoB is formatted YYYY-MM-DDTHH:MM:SS
+    status = await valquest.authenticated_request("POST", f"users/register", token=None, json={"username":username,"email":email,"password":password,"referrer":referrer,"dateOfBirth":DoB,"locality":1,"inviteCode":"","source":""})
+    if int(status[0]) == 200: return True
+    else: return False
 
 # -- NODES --
 # node/name  - Get a primary node identity
@@ -118,6 +140,7 @@ async def get_authenticated_user(token:str): # Returns authenticated json
 
 # -- ECONOMY --
 # eco/accounts/self  - Retrieve economy accounts
+# eco/accounts/self/global  - Retrieve user global account
 
 # -- MODERATION --
 # bans (IssuerId), (PlanetId), (TargetId), (Reason), (TimeCreated), (TimeExpires, null = perma)  - Issue a ban
--- valquest.py
+++ valquest.py
@@ -10,7 +10,7 @@ async def retrieve_node(): # Returns the node name, needed for an authenticated_
     async with aiohttp.ClientSession() as main_session:
         try:
             async with main_session.request("GET", "https://app.valour.gg/api/node/name") as resp:
-                return await resp.text()
+                return await resp.text() # Hackish way of going about this, returns .text() even when != 200
         except Exception as e:
             await logs.error(f"An unexpected error occurred during retrieve_node: {e}")
             return -1
@@ -18,7 +18,9 @@ async def retrieve_node(): # Returns the node name, needed for an authenticated_
 async def authenticated_request(method, url, token, **kwargs): # Returns the status code and response (text/json)
     global node
     while node == -1 or node == None: node = await retrieve_node() # Generally only ran at first initialization
-    async with aiohttp.ClientSession(headers={"authorization": token, "x-server-select": node}) as main_session:
+    if token: headers = {"authorization": token, "x-server-select": node}
+    else: headers={"x-server-select": node}
+    async with aiohttp.ClientSession(headers=headers) as main_session:
         try:
             async with main_session.request(method, BaseAddress + url, **kwargs) as resp:
                 if 'application/json' in resp.headers.get('Content-Type', ''):
